af004a09f8ac7b962ce0755c34e220e1b2dd1e85
Kevin Gadd <kevin.gadd@gmail.com>
2011-06-09 15:46:27 -0700

    Rig up simpletest to a NUnit test runner

---
+++ TestAdapter/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace XPTrace {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void SimpleTest () {
+            var output = Util.RunProcess("simpletest.exe");
+            Assert.AreEqual(
+                new[] {
+                    "0",
+                    "disabled marker hit: 0 'function_with_marker::marker::hit'",
+                    "1", "2",
+                    "marker hit: 0 'function_with_marker::marker::hit'", 
+                    "3", "4"
+                },
+                output.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+            );
+        }
+    }
+}
---
+++ TestAdapter/Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace XPTrace {
+    public static class Util {
+        public static string RunProcess (string filename, string arguments = "") {
+            var psi = new ProcessStartInfo(filename, arguments) {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+
+            var output = new StringBuilder();
+
+            using (var process = Process.Start(psi)) {
+                process.ErrorDataReceived += (sender, e) => {
+                    lock (output)
+                        output.AppendLine(e.Data);
+                };
+                process.OutputDataReceived += (sender, e) => {
+                    lock (output)
+                        output.AppendLine(e.Data);
+                };
+
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+
+                process.WaitForExit();
+            }
+
+            return output.ToString().Trim();
+        }
+    }
+}
