c2e43c4230f42898494fbd915dbf23f38f779f5f
Kevin Gadd <kevin.gadd@gmail.com>
2011-06-20 08:18:33 -0700

    Checkpoint

--- payload-windows/exports.cpp
+++ payload-windows/exports.cpp
@@ -1,10 +1,34 @@
-#include "common.h"
-#include "xptrace.h"
+#include "payload.h"
+#include "windows.h"
 
 using namespace xptrace;
 
-PAYLOAD_EXPORT(void) payload_initialize () {
-}
+HWND g_ownerWindow;
+int g_ipcMessageId;
 
-PAYLOAD_EXPORT(void) payload_shutdown () {
+typedef BOOL (WINAPI *pChangeWindowMessageFilter) (
+  __in UINT message, __in DWORD dwFlag
+);
+
+
+PAYLOAD_EXPORT(void) payload_main (HWND ownerWindow) {
+    g_ownerWindow = ownerWindow;
+    g_ipcMessageId = RegisterWindowMessage(L"xptrace.IPCMessage");
+
+    // Calling PeekMessage once ensures that our thread has a message queue
+    MSG msg;
+    PeekMessage(&msg, 0, g_ipcMessageId, g_ipcMessageId, 0);
+
+    // If our integrity level is higher than that of the tracing control process, we
+    //  must change the UIPI filter so that IPC messages can make it into our queue
+    {
+        HMODULE hModule = LoadLibrary(L"user32.dll");
+
+        // UIPI is still relatively new, so only try to call the API if it's available
+        pChangeWindowMessageFilter pFn = (pChangeWindowMessageFilter)GetProcAddress(hModule, "ChangeWindowMessageFilter");
+        if (pFn)
+            pFn(g_ipcMessageId, MSGFLT_ADD);
+
+        FreeLibrary(hModule);
+    }  
 }
---
+++ payload-windows/payload.h
@@ -0,0 +1,7 @@
+#ifndef __PAYLOAD_H__
+#define __PAYLOAD_H__
+
+#include "common.h"
+#include "xptrace.h"
+
+#endif
